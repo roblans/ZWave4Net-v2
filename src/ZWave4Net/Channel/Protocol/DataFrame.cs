8fa3359fe2c06ffa7fc2459679d58f323d3fb7ce
Rob Lans <r.lans@upcmail.nl>
2018-11-09 23:37:17 +0100

    Dev

--- src/ZWave4Net/Channel/Protocol/DataFrame.cs
+++ src/ZWave4Net/Channel/Protocol/DataFrame.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ZWave4Net.Channel.Protocol
 {
-    public sealed class DataFrame : Frame
+    public sealed class DataFrame : Frame, IEquatable<DataFrame>
     {
         public readonly DataFrameType Type;
         public readonly CommandFunction Function;
@@ -27,6 +27,39 @@ namespace ZWave4Net.Channel.Protocol
         public override string ToString()
         {
             return $"{Header} {Type} {Function}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as DataFrame);
+        }
+
+        public bool Equals(DataFrame other)
+        {
+            return other != null &&
+                   base.Equals(other) &&
+                   Type == other.Type &&
+                   Function == other.Function &&
+                   Parameters.SequenceEqual(other.Parameters);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -2001256323;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + Type.GetHashCode();
+            hashCode = hashCode * -1521134295 + Function.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(DataFrame frame1, DataFrame frame2)
+        {
+            return EqualityComparer<DataFrame>.Default.Equals(frame1, frame2);
+        }
+
+        public static bool operator !=(DataFrame frame1, DataFrame frame2)
+        {
+            return !(frame1 == frame2);
         }
     }
 }
