c16e4da735a6ea4e30c6ad7551bdeb7d6b232bf0
Rob Lans <r.lans@upcmail.nl>
2018-11-13 08:54:47 +0100

    Dev

---
+++ src/ZWave4Net/Channel/Protocol/FrameBroker.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ZWave4Net.Channel.Protocol
+{
+    public class FrameBroker
+    {
+        private readonly ConcurrentDictionary<Action<Frame>, object> _subscribers = new ConcurrentDictionary<Action<Frame>, object>();
+        private FrameReader _reader;
+        private FrameWriter _writer;
+        private Task _task;
+        public readonly CancellationToken Cancelation;
+
+        public FrameBroker(IByteStream stream, CancellationToken cancelation)
+        {
+            _reader = new FrameReader(stream);
+            _writer = new FrameWriter(stream);
+
+            Cancelation = cancelation;
+        }
+
+        private Task Publish(Action<Action<Frame>> action)
+        {
+            return Task.Run(() => Parallel.ForEach(_subscribers, (subscriber) => action(subscriber.Key)));
+        }
+
+        public void Start()
+        {
+            if (_task != null)
+                throw new InvalidOperationException("Broker already started");
+
+            _task = Task.Run(async () =>
+            {
+                while (!Cancelation.IsCancellationRequested)
+                {
+                    try
+                    {
+                        var frame = await _reader.Read(Cancelation);
+                        if (frame == null)
+                            break;
+
+                        if (frame is EventDataFrame eventDataFrame)
+                        {
+                            await _writer.Write(Frame.ACK, Cancelation);
+                        }
+
+                        await Publish((subscriber) => subscriber(frame));
+                    }
+                    catch(Exception ex)
+                    {
+                        // todo
+                    }
+                }
+            }, Cancelation);
+        }
+
+        public bool Wait(TimeSpan timeout)
+        {
+            if (_task != null)
+            {
+                return _task.Wait(timeout);
+            }
+            return true;
+        }
+
+        public Task Send(RequestDataFrame request)
+        {
+            var completion = new TaskCompletionSource<bool>();
+
+            var subscriber = default(IDisposable);
+            subscriber = Subscribe(async (response) =>
+            {
+                if (response == Frame.ACK)
+                {
+                    completion.TrySetResult(true);
+                    subscriber.Dispose();
+                }
+                await _writer.Write(request, Cancelation);
+            });
+
+            return completion.Task;
+        }
+
+        private void Unsubscribe(Action<Frame> subscriber)
+        {
+            _subscribers.TryRemove(subscriber, out _);
+        }
+
+        public IDisposable Subscribe(Action<Frame> subscriber)
+        {
+            _subscribers.TryAdd(subscriber, null);
+            return new Unsubscriber(subscriber, (item) => Unsubscribe(item));
+        }
+
+        private class Unsubscriber : IDisposable
+        {
+            private readonly Action<Frame> _subscriber;
+            private readonly Action<Action<Frame>> _onUnsubscribe;
+
+            public Unsubscriber(Action<Frame> subscriber, Action<Action<Frame>> onUnsubscribe)
+            {
+                _subscriber = subscriber;
+                _onUnsubscribe = onUnsubscribe;
+            }
+
+            public void Dispose()
+            {
+                _onUnsubscribe(_subscriber);
+            }
+        }
+    }
+}
--- src/ZWave4Net/Channel/ZWaveChannel.cs
+++ src/ZWave4Net/Channel/ZWaveChannel.cs
@@ -6,10 +6,9 @@ using ZWave4Net.Channel.Protocol;
 
 namespace ZWave4Net.Channel
 {
-    public class ZWaveChannel : IObserver<Frame>
+    public class ZWaveChannel
     {
-        private readonly FrameReceiver _receiver;
-        private readonly FrameTransmitter _transmitter;
+        private readonly FrameBroker _frameBroker;
 
         private readonly CancellationTokenSource _cancellationSource;
         public readonly IByteStream Stream;
@@ -20,40 +19,19 @@ namespace ZWave4Net.Channel
 
             _cancellationSource = new CancellationTokenSource();
 
-            var reader = new FrameReader(Stream);
-            var writer = new FrameWriter(stream);
-
-            _receiver = new FrameReceiver(reader, _cancellationSource.Token);
-            _transmitter = new FrameTransmitter(writer, _cancellationSource.Token);
+            _frameBroker = new FrameBroker(stream, _cancellationSource.Token);
         }
 
         public void Open()
         {
-            _receiver.Start();
+            _frameBroker.Start();
         }
 
         public void Close()
         {
             _cancellationSource.Cancel();
 
-            _receiver.Wait(TimeSpan.FromSeconds(1));
-            _transmitter.Wait(TimeSpan.FromSeconds(1));
-        }
-
-        void IObserver<Frame>.OnCompleted()
-        {
-        }
-
-        void IObserver<Frame>.OnError(Exception error)
-        {
-        }
-
-        void IObserver<Frame>.OnNext(Frame value)
-        {
-            if (value is EventDataFrame eventData)
-            {
-                _transmitter.Transmit(Frame.ACK);
-            }
+            _frameBroker.Wait(TimeSpan.FromSeconds(1));
         }
     }
 }
