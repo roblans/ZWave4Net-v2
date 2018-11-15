5e3066804e4f96b1cdc8ed75c3a5e65dfc2de861
Rob Lans <r.lans@upcmail.nl>
2018-11-15 13:56:11 +0100

    Dev

--- src/ZWave4Net/Channel/Protocol/FrameReader.cs
+++ src/ZWave4Net/Channel/Protocol/FrameReader.cs
@@ -41,27 +41,37 @@ namespace ZWave4Net.Channel.Protocol
 
         private async Task<DataFrame> ReadDataFrame(CancellationToken cancelation)
         {
-            // read the length
-            var length = await Stream.ReadByte(cancelation);
+            // INS12350-Serial-API-Host-Appl.-Prg.-Guide | 6.2.1 Data frame reception timeout
+            // A receiving host or Z-Wave chip MUST abort reception of a Data frame if the reception has lasted for 
+            // more than 1500ms after the reception of the SOF byte.
+            using (var timeoutCancelation = new CancellationTokenSource(1500))
+            {
+                // combine the passed and the timeout cancelationtokens  
+                using (var linkedCancellation = CancellationTokenSource.CreateLinkedTokenSource(cancelation, timeoutCancelation.Token))
+                {
+                    // read the length
+                    var length = await Stream.ReadByte(cancelation);
 
-            // read data (payload and checksum)
-            var data = await Stream.Read(length, cancelation);
+                    // read data (payload and checksum)
+                    var data = await Stream.Read(length, cancelation);
 
-            // payload (data without checksum)
-            var payload = data.Take(data.Length - 1).ToArray();
+                    // payload (data without checksum)
+                    var payload = data.Take(data.Length - 1).ToArray();
 
-            // checksum
-            var actualChecksum = data.Last();
+                    // checksum
+                    var actualChecksum = data.Last();
 
-            // calculate required checksum (include length)
-            var expectedChecksum = new byte[] { length }.Concat(payload).CalculateChecksum();
+                    // calculate required checksum (include length)
+                    var expectedChecksum = new byte[] { length }.Concat(payload).CalculateChecksum();
 
-            // validate checksum
-            if (actualChecksum != expectedChecksum)
-                throw new ChecksumException("Checksum failure");
+                    // validate checksum
+                    if (actualChecksum != expectedChecksum)
+                        throw new ChecksumException("Checksum failure");
 
-            // return dataframe
-            return new DataFrame((DataFrameType)payload[0], payload.Skip(1).ToArray());
+                    // return dataframe
+                    return new DataFrame((DataFrameType)payload[0], payload.Skip(1).ToArray());
+                }
+            }
         }
     }
 }
--- src/ZWave4Net/Channel/Protocol/MessageBroker.cs
+++ src/ZWave4Net/Channel/Protocol/MessageBroker.cs
@@ -15,6 +15,7 @@ namespace ZWave4Net.Channel.Protocol
         private readonly FrameReader _reader;
         private readonly FrameWriter _writer;
         private readonly ValueChangedEvent<Frame> _lastFrameEvent = new ValueChangedEvent<Frame>(null);
+        private readonly SemaphoreSlim _sendLock = new SemaphoreSlim(1, 1);
         private Task _task;
 
         public readonly CancellationToken Cancelation;
@@ -76,15 +77,58 @@ namespace ZWave4Net.Channel.Protocol
 
         public async Task Send(RequestMessage message)
         {
-            while (true)
+            // INS12350-Serial-API-Host-Appl.-Prg.-Guide | 6.5.2 Request/Response frame flow
+            // Note that due to the simple nature of the simple acknowledge mechanism, only one REQ->RES session is allowed.
+            await _sendLock.WaitAsync(Cancelation);
+            try
+            { 
+                // number of retransmissions
+                var retransmissions = 0;
+
+                while (true)
+                {
+                    // reset last frame receveid event
+                    _lastFrameEvent.Reset();
+
+                    // send de request
+                    await _writer.Write((Frame)message, Cancelation);
+
+                    // INS12350-Serial-API-Host-Appl.-Prg.-Guide | 5.1 ACK frame
+                    // The host MUST wait for a period of 1500ms before timing out waiting for the ACK frame
+                    using (var waitCancelation = new CancellationTokenSource(1500))
+                    {
+                        // wait for new frame (with timeout)
+                        var response = await _lastFrameEvent.Wait(waitCancelation.Token);
+
+                        // ACK received, so where done 
+                        if (response == Frame.ACK)
+                            break;
+
+                        // other options: CAN or NACK received
+                        if (response == Frame.CAN || response == Frame.NAK)
+                        {
+                            // INS12350-Serial-API-Host-Appl.-Prg.-Guide | 6.3 Retransmission
+                            // A host or Z-Wave chip MUST NOT carry out more than 3 retransmissions
+                            if (retransmissions > 3)
+                            {
+                                // translate to exceptions
+                                if (response == Frame.CAN)
+                                    throw new CanResponseException();
+                                if (response == Frame.NAK)
+                                    throw new NakResponseException();
+                            }
+
+                            // INS12350-Serial-API-Host-Appl.-Prg.-Guide | 6.3 Retransmission
+                            // Twaiting = 100ms + n*1000ms 
+                            // where n is incremented at each retransmission. n = 0 is used for the first waiting period.
+                            await Task.Delay(100 + retransmissions++ * 1000);
+                        }
+                    }
+                }
+            }
+            finally
             {
-                _lastFrameEvent.Reset();
-
-                await _writer.Write((Frame)message, Cancelation);
-
-                var response = await _lastFrameEvent.Wait();
-                if (response == Frame.ACK)
-                    break;
+                _sendLock.Release();
             }
         }
     }
