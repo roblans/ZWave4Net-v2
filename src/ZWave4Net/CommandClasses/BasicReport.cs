706b8545921714363cc6507fd6f34dff91ba59b5
Rob Lans <r.lans@upcmail.nl>
2018-12-21 15:31:45 +0100

    Dev

--- src/ZWave4Net/CommandClasses/BasicReport.cs
+++ src/ZWave4Net/CommandClasses/BasicReport.cs
@@ -4,8 +4,14 @@ using System.Text;
 
 namespace ZWave4Net.CommandClasses
 {
+    /// <summary>
+    /// Advertises the status of the primary functionality of the node
+    /// </summary>
     public class BasicReport : Report
     {
+        /// <summary>
+        /// The current value
+        /// </summary>
         public byte Value { get; private set; }
 
         protected override void Read(PayloadReader reader)
